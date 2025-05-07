cfe6dfefd640ff388faf60a6f35a9e1b65cd8ee6
aman-deep940 <amandeep.vdoit@gmail.com>
2025-05-07 16:52:21 +0530

    applied signUp

--- controllers/logginController.js
+++ controllers/logginController.js
@@ -32,6 +32,43 @@ const signUp = async (req, res) => {
   }
 };
 
+const logIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const User = await user.findOne({
+      email,
+    });
+
+    if (!User) {
+      res.status(400).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcyrpt.compare(password, User.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign({ id: User._id, email: User.email }, process.env.JWT_SECRET);
+
+    console.log(process.env.JWT_SECRET);
+
+    res.status(200).json({
+      token: token,
+    });
+  } catch (error) {
+    console.log("Login Error", error);
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
+
 module.exports = {
   signUp: signUp,
+  logIn: logIn,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { signUp } = require("../controllers/logginController");
+const { signUp, logIn } = require("../controllers/logginController");
 
 router.post("/signup", signUp);
+router.post("/login", logIn);
 
 module.exports = router;
