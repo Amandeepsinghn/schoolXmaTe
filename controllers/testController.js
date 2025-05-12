867aefbb3ed9e83e11ee78122387384866a12b75
aman-deep940 <amandeep.vdoit@gmail.com>
2025-05-12 15:26:14 +0530

    added testGeneration

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const { test } = require("../models/test");
 
+const { testGeneration } = require("../utils/testGeneration");
+
 const testData = async (req, res) => {
   try {
     const userId = req.user;
@@ -21,6 +23,34 @@ const testData = async (req, res) => {
   }
 };
 
+const genTest = async (req, res) => {
+  try {
+    const userId = req.user;
+    const { subject, title, difficulty } = req.body;
+
+    const testData = await testGeneration(subject, title, difficulty);
+
+    const newTitle = testData.title;
+
+    const questionData = testData.questions;
+
+    await test.insertMany({
+      title: newTitle,
+      seriesData: questionData,
+      userId: userId,
+    });
+
+    return res.status(200).json({
+      message: "successfully inserted the test.",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error,
+    });
+  }
+};
+
 module.exports = {
   testData,
+  genTest,
 };
--- routes/test.js
+++ routes/test.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { auth } = require("../middleware/authMiddleware");
-const { testData } = require("../controllers/testController");
+const { testData, genTest } = require("../controllers/testController");
 
 router.get("/data", auth, testData);
+router.post("/insertTest", auth, genTest);
 
 module.exports = router;
-
-
-
-
-
-
