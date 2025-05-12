37727e55e53f165d1e262c681ad67028b43b5644
Annuvrat <annuvrat1098@gmail.com>
2025-05-12 12:48:04 +0530

    history route done

--- controllers/history.controller.js
+++ controllers/history.controller.js
@@ -3,14 +3,12 @@ const { history } = require("../models/history");
 
 const getAllHistory = async (req, res) => {
   try {
-  
-    const userId = req.user;
+    const userId = req.user.id;
     console.log(userId);
     
-    const histories = await history.find({ userId: userId })
+    const histories = await history.find({ userId })
       .populate('testId', 'title') 
       .populate('userId', 'name email'); 
-
     res.status(200).json(histories);
   } catch (err) {
     res.status(500).json({ error: err.message });
